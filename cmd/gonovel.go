53b3586ca9fda0fd4135e43bcfbf31cec308e46d
Korman <korman@qq.com>
2021-03-02 09:50:44 +0800

    ok

--- cmd/gonovel.go
+++ cmd/gonovel.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	global "gonovel/internal"
-	"gonovel/pkg/node"
+	"flag"
+	"fmt"
+	"gonovel/pkg/book"
+	"time"
 )
 
 func main() {
-	node := node.CreateNode(global.Volume)
-	println(node.Brief())
+	path := flag.String("path", "tests/txt_files/test_01.txt", "输入路径")
+
+	flag.Parse()
+
+	println(fmt.Sprintf("准备加载%s...", *path))
+
+	t := time.Now()
+	book, err := book.CreateBook(*path)
+
+	if nil != err {
+		println(err)
+	}
+
+	if nil == book {
+
+	}
+
+	elapsed := time.Since(t)
+
+	println(fmt.Sprintf("耗时%s秒", elapsed))
 }
